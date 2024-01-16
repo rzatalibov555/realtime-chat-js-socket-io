61f3d2f74d43beffd0d9968cac2476603a17ad8c
rzatalibov555 <sethubpro@gmail.com>
2024-01-17 01:58:25 +0400

    worked. Fixed a bug

--- public/code.js
+++ public/code.js
@@ -1,11 +1,11 @@
 (function(){
     const app = document.querySelector(".app");
-    const socket = io()
+    const socket = io();
 
     let uname;
 
-    app.querySelector(".join-screen #joun-user").addEventListener("click", function(){
-        let username = app.querySelector("join-screen #username").value;
+    app.querySelector(".join-screen #join-user").addEventListener("click", function(){
+        let username = app.querySelector(".join-screen #username").value;
         if(username.length == 0){
             return;
         }
@@ -18,7 +18,7 @@
 
 
     app.querySelector(".chat-screen #send-message").addEventListener("click", function(){
-        let message = app.querySelector("chat-screen #message-input").value;
+        let message = app.querySelector(".chat-screen #message-input").value;
         if(message.length == 0){
             return;
         }
--- server.js
+++ server.js
@@ -4,20 +4,16 @@ const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
 
-
 // Creating socket and listen
 
 // start 1
 const io = require("socket.io")(server);
 // end
 
-
-
-
 app.use(express.static(path.join(__dirname+"/public")));
 
 // start2
-io.on("connection", function(socket)){
+io.on("connection", function(socket){
     socket.on("newuser",function(username){
         socket.broadcast.emit("update", username + " joined the conversation");
     });
@@ -29,7 +25,7 @@ io.on("connection", function(socket)){
     socket.on("chat",function(message){
         socket.broadcast.emit("chat", message);
     });
-}
+})
 // end
 
 server.listen(5000);
